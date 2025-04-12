2285495c13820d4c68dd1e53c25cc65a1be1276d
jayasurya0007 <115530104+jayasurya0007@users.noreply.github.com>
2025-04-12 22:55:28 +0530

    Modified the sitemap.xml

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -3,7 +3,7 @@ import { createWriteStream } from 'fs';
 
 const sitemap = new SitemapStream({ hostname: 'https://jayasuryar.xyz' });
 
-const pages = ['/', '/about', '/projects', '/contact']; // update with your pages
+const pages = ['/',]; // update with your pages
 
 pages.forEach(page => {
   sitemap.write({ url: page, changefreq: 'weekly', priority: 0.8 });
